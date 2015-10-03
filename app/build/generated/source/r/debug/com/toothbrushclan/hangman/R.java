7f507fdfdacdad3116dd5eea4e2bf96141565e2f
Umar Shaikh <umar.shaikh@gmail.com>
2015-10-03 17:36:45 +0530

    small checkin

--- app/build/generated/source/r/debug/com/toothbrushclan/hangman/R.java
+++ app/build/generated/source/r/debug/com/toothbrushclan/hangman/R.java
@@ -15,16 +15,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
-        public static final int hangman=0x7f020000;
-        public static final int hangman1=0x7f020001;
-        public static final int hangman2=0x7f020002;
-        public static final int hangman3=0x7f020003;
-        public static final int hangman4=0x7f020004;
-        public static final int hangman5=0x7f020005;
-        public static final int hangman6=0x7f020006;
-        public static final int hangman7=0x7f020007;
-        public static final int hangman8=0x7f020008;
-        public static final int hangman9=0x7f020009;
+        public static final int hangman1=0x7f020000;
+        public static final int hangman2=0x7f020001;
+        public static final int hangman3=0x7f020002;
+        public static final int hangman4=0x7f020003;
+        public static final int hangman5=0x7f020004;
+        public static final int hangman6=0x7f020005;
+        public static final int hangman7=0x7f020006;
+        public static final int hangman8=0x7f020007;
+        public static final int hangman9=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0023;
