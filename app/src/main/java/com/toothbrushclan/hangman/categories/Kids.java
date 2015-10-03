3923cb5140fce882cbf1b089eea3da5514a6262b
Umar Shaikh <umar.shaikh@gmail.com>
2015-10-03 17:35:52 +0530

    Fix compilation issue

--- app/src/main/java/com/toothbrushclan/hangman/categories/Kids.java
+++ app/src/main/java/com/toothbrushclan/hangman/categories/Kids.java
@@ -1,17 +1,11 @@
 package com.toothbrushclan.hangman.categories;
 
 import android.content.Context;
-import android.content.res.Resources;
-
-import com.toothbrushclan.hangman.utilities.Helper;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.ListIterator;
 
 /**
